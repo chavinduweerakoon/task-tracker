b5211c7c7213e2ec8c66ba8873437198058b506b
chavindu weerakoon <chavinduweerakoon@gmail.com>
2021-08-08 23:40:14 +0530

    submit & create task functionality

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-add-task',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-task.component.css'],
 })
 export class AddTaskComponent implements OnInit {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
   text!: string;
   day!: string;
   reminder: boolean = false;
@@ -18,5 +20,17 @@ export class AddTaskComponent implements OnInit {
       alert('Please add a task!');
       return;
     }
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder,
+    };
+
+    this.onAddTask.emit(newTask);
+
+    //todo - emit event
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
   }
 }
